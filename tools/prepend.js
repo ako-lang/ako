a0319ebd54732efa46d762eb939089e29732f6d5
Kefniark <kevin@destrem.net>
2021-04-27 11:51:24 +0900

    Update package.json

--- tools/prepend.js
+++ tools/prepend.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-let code = fs.readFileSync('./dist/ako.js')
+let code = fs.readFileSync('./dist/node/ako.js')
 
 code = `#!/usr/bin/env node
 /**
  * AKO Language CLI !
  */
 ${code}`
-fs.writeFileSync('./dist/ako.js', code)
+fs.writeFileSync('./dist/node/ako.js', code)
