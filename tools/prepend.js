e93aeaaaa548aeb6634e5e2f9dc488c95a09385c
Kefniark <kevin@destrem.net>
2021-04-30 13:56:10 +0900

    Export typings for the different flavor

--- tools/prepend.js
+++ tools/prepend.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-let code = fs.readFileSync('./dist/node/ako.js')
+let code = fs.readFileSync('./dist/ako-cli.js')
 
 code = `#!/usr/bin/env node
 /**
  * AKO Language CLI !
  */
 ${code}`
-fs.writeFileSync('./dist/node/ako.js', code)
+fs.writeFileSync('./dist/ako-cli.js', code)
