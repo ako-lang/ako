04865394e8f7eb5e61799a28ed3e6aaee459186a
Kefniark <kevin@destrem.net>
2021-04-30 14:14:09 +0900

    Create dist folder if not exist yet

--- tools/gen-typings.js
+++ tools/gen-typings.js
@@ -2,4 +2,6 @@ const fs = require('fs')
 const path = require('path')
 
 const file = process.argv[2]
+
+if (!fs.existsSync('dist')) fs.mkdirSync('dist')
 fs.writeFileSync(path.join('dist', `${file}.d.ts`), `export * from '../typings/src/dist/${file}'`)
