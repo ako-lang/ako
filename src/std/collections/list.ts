d3f764904b6f353378a09bfee9537c4c81ab5e67
Kefniark <kevin@destrem.net>
2021-05-04 16:51:37 +0900

    Fix the default compare sort issue

--- src/std/collections/list.ts
+++ src/std/collections/list.ts
@@ -6,7 +6,13 @@ export const list = {
     return arr.map(lambda)
   },
   'List.sort': (arr: number[], lambda: (item1: number, item2: number) => number): number[] => {
-    return lambda ? [...arr].sort(lambda) : [...arr].sort()
+    return lambda
+      ? [...arr].sort(lambda)
+      : [...arr].sort((a, b) => {
+          if (a < b) return -1
+          if (a > b) return 1
+          return 0
+        })
   },
   'List.reverse': (arr: number[]): number[] => {
     return [...arr].reverse()
